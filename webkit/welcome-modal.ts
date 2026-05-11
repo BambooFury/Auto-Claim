37f8c8d146aff3ac4a9d5f0c537273350b0fe733
BambooFury <168723703+BambooFury@users.noreply.github.com>
2026-05-11 16:12:16 +0300

    docs(welcome): bump to v5, clarify Games vs All filter modes

--- webkit/welcome-modal.ts
+++ webkit/welcome-modal.ts
@@ -4,7 +4,7 @@ import {
   WELCOME_HTML_TEMPLATE,
 } from './_assets.generated';
 
-const SEEN_FLAG = 'fgg_welcomed_v4';
+const SEEN_FLAG = 'fgg_welcomed_v5';
 
 function alreadySeen(): boolean {
   try { return localStorage.getItem(SEEN_FLAG) === '1'; }
